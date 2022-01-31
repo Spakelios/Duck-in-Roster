1e9f0842b5bf7c57b5b2de297aaa8963b1e1de10
JoelleT02 <77802831+JoelleT02@users.noreply.github.com>
2022-01-31 19:59:37 +0000

    WOAH DUCKIE WATCH OUT IT'S A LOG

--- Duck in Roster/Assets/Kellie's Stuff/Code/PlayerMovement.cs
+++ Duck in Roster/Assets/Kellie's Stuff/Code/PlayerMovement.cs
@@ -11,6 +11,12 @@ public class PlayerMovement : MonoBehaviour
     public GameObject duckie;
     public GameObject duckie2;
     public GameObject duckie3;
+    public bool duckie1Obtained = false;
+    public bool duckie2Obtained = false;
+    public bool duckie3Obtained = false;
+    private GameObject duckling1;
+    private GameObject duckling2;
+    private GameObject duckling3;
 
     private void FixedUpdate()
     {
@@ -32,8 +38,9 @@ public class PlayerMovement : MonoBehaviour
 
             switch (ScoreManager.score)
             {
-                case 1:
-                    Instantiate(duckie, transform.position, transform.rotation);
+                case 1 when duckie1Obtained == false:
+                    duckling1 = Instantiate(duckie, transform.position, transform.rotation);
+                    duckie1Obtained = true;
                     break;
                 /*
             for (int i = 0; i < 10; i++)
@@ -42,14 +49,38 @@ public class PlayerMovement : MonoBehaviour
                 FollowTarget.offset = new Vector3(0, 0, i * -20f);
             }
             */
-                case 2:
-                    Instantiate(duckie2, transform.position, transform.rotation);
+                case 2 when duckie2Obtained == false:
+                    duckling2 = Instantiate(duckie2, transform.position, transform.rotation);
+                    duckie2Obtained = true;
                     break;
-                
-                case 3:
-                    Instantiate(duckie3, transform.position, transform.rotation);
+
+                case 3 when duckie3Obtained == false:
+                    duckling3 = Instantiate(duckie3, transform.position, transform.rotation);
+                    duckie3Obtained = true;
                     break;
             }
         }
+
+        if (!other.CompareTag("Obstacle")) return;
+        switch (ScoreManager.score)
+        {
+            case 2 when duckie3Obtained:
+                Destroy(duckling3);
+                duckie3Obtained = false;
+                break;
+            case 1 when duckie2Obtained:
+                Destroy(duckling2);
+                duckie2Obtained = false;
+                break;
+            case 0 when duckie1Obtained:
+                Destroy(duckling1);
+                duckie1Obtained = false;
+                break;
+            case -1:
+                Destroy(gameObject);
+                break;
+        }
     }
 }
+
+    
