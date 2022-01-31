811e33fe52ffec21b89fa3c39bc18564e0bd84c2
Spakelios <73300254+Spakelios@users.noreply.github.com>
2022-01-31 18:32:10 +0000

    gaming gamers

---
+++ Duck in Roster/Assets/Kellie's Stuff/Code/NewGame.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewGame : MonoBehaviour
+{
+    public void Scene1() 
+    {  
+        SceneManager.LoadScene("PROTOTYPE SPEED 1"); 
+        
+    }
+
+    public void Scene2()
+    { 
+        Application.Quit();
+    }
+
+    public void Scene3()
+    {
+        SceneManager.LoadScene("PROTOTYPE SPEED 2");
+    }
+}
+
